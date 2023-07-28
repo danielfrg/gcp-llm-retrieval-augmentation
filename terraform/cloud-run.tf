6210bb779680d28d13d01678227e3e9c9eaaecdf
Daniel Rodriguez <daniel@danielfrg.com>
2023-07-28 14:07:23 -0500

    Add permission to cloud run to query Matching Engine

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -27,14 +27,36 @@ resource "google_cloudbuild_trigger" "include-build-logs-trigger" {
 
 # Create the Cloud Run Service
 
+resource "google_service_account" "cloudrun-api" {
+  account_id   = "retreival-aug-cloudrun-api"
+  display_name = "Service Account for the Retrieval Augmentation Cloud Run API"
+}
+
+resource "google_project_iam_member" "sa-ai-user-binding" {
+  project = var.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_service_account.cloudrun-api.email}"
+}
+
 resource "google_cloud_run_service" "api" {
   name     = "retreival-augmentation-api"
   location = var.region
 
   template {
     spec {
+      service_account_name = google_service_account.cloudrun-api.email
       containers {
         image = "us-central1-docker.pkg.dev/langchain-llmops/retrieval-augmentation-api/api"
+        resources {
+          limits = {
+            memory = "2Gi"
+            cpu    = "1000m"
+          }
+          requests = {
+            memory = "2Gi"
+            cpu    = "1000m"
+          }
+        }
       }
     }
   }
